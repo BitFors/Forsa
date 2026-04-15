8dd2bc30daff9662692e59a8571cef431b45d02e
AdamT20054 <github@adamt05.com>
2026-04-15 02:50:42 +0100

    feat: enhance bencode value enum and add utility constructors
    Added new `dict` and `list` constructors for easier creation of `Dict` and `List` types. Renamed `BencodeValue` to `Value` with updated variants.

--- src/bencode/value.rs
+++ src/bencode/value.rs
@@ -1,9 +1,35 @@
 use bytes::Bytes;
 use indexmap::IndexMap;
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum BencodeValue {
-    Integer(i64), // Technically there's no upper limit, in practice it should fit in 64bits.
+pub enum Value {
+    Integer(i64), // Technically there's no upper limit, i reckon it should fit in 64bits but I'd imagine this'll change in the future..
     Bytes(Vec<u8>),
-    List(Vec<BencodeValue>),
-    Dict(IndexMap<Bytes, BencodeValue>),
+    List(Vec<Self>),
+    Dict(IndexMap<Bytes, Self>),
+}
+
+impl Value {
+    /// Create a dict from key-value pairs
+    pub fn dict<I, K, V>(items: I) -> Self
+    where
+        I: IntoIterator<Item = (K, V)>,
+        K: Into<Bytes>,
+        V: Into<Self>,
+    {
+        let mut map: IndexMap<Bytes, Self> = items
+            .into_iter()
+            .map(|(k, v)| (k.into(), v.into()))
+            .collect();
+        map.sort_keys();
+        Self::Dict(map)
+    }
+
+    /// Create a list from an iterator of values
+    pub fn list<I, V>(items: I) -> Self
+    where
+        I: IntoIterator<Item = V>,
+        V: Into<Value>,
+    {
+        Value::List(items.into_iter().map(Into::into).collect())
+    }
 }
