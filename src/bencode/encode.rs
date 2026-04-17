91090baf85a25e2fdb1e81b5ccb47dff09a149bf
AdamT20054 <github@adamt05.com>
2026-04-18 00:34:44 +0100

    feat: add error handling and skeleton for bencode encoding
    Signed-off-by: AdamT20054 <github@adamt05.com>

--- src/bencode/encode.rs
+++ src/bencode/encode.rs
@@ -1,1 +1,16 @@
 //TODO
+
+use super::Value;
+
+pub fn encode(value: &Value) -> Vec<u8> {
+    //TODO Might change after impl
+    let mut encoder = Encoder::new();
+    encoder.encode(value);
+    encoder.into_bytes()
+}
+
+pub struct Encoder {
+    buf: Vec<u8>, //TODO might change after impl
+}
+
+impl Encoder {}
--- src/bencode/error.rs
+++ src/bencode/error.rs
@@ -1,1 +1,14 @@
+//TODO
 use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Decode(#[from] DecodeError),
+}
+
+#[derive(Debug, Error)]
+pub enum DecodeError {
+    #[error("Temp")]
+    Temp,
+}
